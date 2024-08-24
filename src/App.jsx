c92d26c10a7629afd1d247a8668c1950582031b3
Gabobix04 <gacevedon@unicartagena.edu.co>
2024-08-24 12:08:40 -0500

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+import '@fontsource-variable/saira';
+
+function App() {
+  
+  return (
+    <h1 className="text-8xl font-normal text-red-400">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
